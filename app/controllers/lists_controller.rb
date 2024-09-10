c1080e06717e512a24c16fc09c19f3fbd2f83651
EC2 Default User <ec2-user@ip-172-31-5-182.us-east-2.compute.internal>
2024-09-10 05:27:03 +0000

    [Finish]3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,17 +8,26 @@ class ListsController < ApplicationController
     
     list.save
     
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end 
   
   private
   #ストロングパラメータ
