f00f0b85ed2fa7889dec1257eb91ab9c45569860
EC2 Default User <ec2-user@ip-172-31-5-182.us-east-2.compute.internal>
2024-09-11 05:31:45 +0000

    [Finish]3-12

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,11 +4,17 @@ class ListsController < ApplicationController
   end
   
   def create
+    #1.データを受け取り新規登録するためのインスタンス作成
     @list = List.new(list_params)
+    #2. データをデータベースに保存するためのsavメソッド実行
     if @list.save
+      #3.フラッシュメッセージを定義し、詳細画面へリダイレクト
+      flash[:notice] = "投稿に成功しました。"
       redirect_to list_path(@list.id)
     else
-      redirect_to new_list_path #renderではなくredirect_toだった場合
+      #3. フラッシュメッセージを定義し、new.html.erbを描画する
+      flash.now[:alert] = "投稿に失敗しました。" #キーをalertに変更
+      render :new
     end
     
   end
